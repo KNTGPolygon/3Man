76176d237e9527024e8151270fbe82e7d7167abe
Aoyama <babybornas1@wp.pl>
2013-03-24 13:26:32 +0100

    Tiles
    added changed tile.h and tile.cpp

--- Tile.cpp
+++ Tile.cpp
@@ -1,8 +1,10 @@
 #include "Tile.h"
 
-Tile::Tile(int _type)
+Tile::Tile(int _type, int pos_x, int pos_y)
 {
 	type = _type;
+	drawingPosition.x = pos_x;
+	drawingPosition.y = pos_y;
 
 }
 
@@ -13,6 +15,26 @@ Tile::Tile()
 
 void Tile::loadSetOfGraphics(const std::string& filename)
 {
-	MyTexture.LoadFromFile( "Data/Textures/something.bmp" );
-	MyTexture.CreateMaskFromColor(sf::Color(255,0,255));
+	tileTexture = ImageManager::getInstance()->loadImage( filename );
+	tileTexture.CreateMaskFromColor(sf::Color(255,0,255));
+
+	//tileSprite.SetImage( tileTexture );
+   // tileSprite.SetScale( 1, 1 ); 
+	//tileSprite.SetPosition( 0, 0 );
+}
+
+Tile::Tile(int _type, const std::string& filename)
+{
+	type = _type;
+	loadSetOfGraphics(filename);
+}
+
+int Tile::getType()
+{
+	return type;
+}
+
+sf::Vector2i Tile::getPosition()
+{
+	return drawingPosition;
 }
--- Tile.h
+++ Tile.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include "ImageManager.h"
+#include <iostream>
 
 #ifndef TILE_H
 #define TILE_H
@@ -8,13 +10,17 @@
 class Tile{
 public:
 	int type;
-	sf::Sprite fieldSprite;				
-	sf::Image MyTexture;
+	//sf::Sprite tileSprite;				
+	sf::Image tileTexture;
+	sf::Vector2i drawingPosition;
 
 public:
-	Tile(int _type);
+	Tile(int _type, int pos_x = 0, int pos_y = 0);
+	Tile(int _type, const std::string& filename);
 	Tile();
 	void loadSetOfGraphics(const std::string& filename);
+	int getType();
+	sf::Vector2i getPosition();
 
 };
 
